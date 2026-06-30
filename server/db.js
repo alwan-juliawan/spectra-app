e9dffadfeffb128a119608a26a5aa9e153ed127e
Alwan Juliawan <alwanjuliawan02@gmail.com>
2026-06-30 10:00:25 +0700

    feat: SQLite persistent volume support + Fly.io deploy config

--- server/db.js
+++ server/db.js
@@ -3,7 +3,10 @@ import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const db = new Database(join(__dirname, "spectra.db"));
+const DB_PATH = process.env.FLY_VOLUME_PATH
+  ? join(process.env.FLY_VOLUME_PATH, "spectra.db")
+  : join(__dirname, "spectra.db");
+const db = new Database(DB_PATH);
 
 db.pragma("journal_mode = WAL");
 db.pragma("foreign_keys = ON");
